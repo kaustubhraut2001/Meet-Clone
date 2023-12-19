31a0b9d0a3675c90718c0a14ef84dc324e33d505
kaustubhr2001@gmail.com <87487138+kaustubhraut2001@users.noreply.github.com>
2023-12-19 20:07:03 +0530

    some css added

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,23 @@
-import { useSocket } from "@/context/socket"
-import { useEffect } from "react";
-import usePeer from "@/hooks/usePeers";
+import styles from "../styles/home.module.css";
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
 
-  const socket = useSocket();
-   usePeer();
-
-  useEffect (() => {
-
-    socket?.on('connection_error', async(err) => {
-      console.log(err.message);
-
-      await fetch(
-        '/api/socket'
-      );
-    });
-
-
-  } , [socket]);
+  const createRoom = () => {
+    const id = uuidv4();
+    router.push(`/${id}`);
+  };
 
   return (
-    <h4>Next app</h4>
-
-  )
+    <div className={styles.homeContainer}>
+      <h4>Meet</h4>
+      <div className={styles.enterRoom}>
+        <input type="text" />
+        <button>Join Room</button>
+      </div >
+      <span className={styles.separatorText}>------------</span>\<button> Create Room</button>
+    </div>
+  );
 }
